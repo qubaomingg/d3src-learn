f72d861154bbfee232a81af89adfa7c1a4f202f3
675861708@qq.com <qubaoming@didichuxing.com>
2017-06-04 22:24:36 +0800

    file server

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var express = require('express');
-var server = express();
-server.use('/', express.static(__dirname + '/'));
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
 server.listen(8010);
+console.log('server on http://127.0.0.1:8010')
